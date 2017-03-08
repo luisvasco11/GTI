8f4ea7fe0e55fdeb7d568944256cb8e5f45103bf
daniel.saldarriaga <daniel.saldarriaga@LMDEADM0332412.da.arus.com.co.>
2017-03-08 16:10:46 -0500

    Se crea el formulario de registro de ausentismo

--- pages/components/registro_ausentismo.php
+++ pages/components/registro_ausentismo.php
@@ -1,2 +1,67 @@
-<?php
-echo "hola";
+
+<form method="post" action="#">
+<div class="row">
+	<div class="col-md-12">
+
+		<div class="box box-danger">
+			<div class="box-header"><br>
+				<h3 class="box-title">Registro de Ausentismo</h3><br><br>
+			</div>	
+			<div class="box-body">               
+                <div class="form-group">
+                <label>Tipo de ausentismo</label>
+                <div class="input-group">
+					<div class="input-group-addon">
+                  <i class="fa fa-list-alt"></i> 
+                  </div>
+                <select id="ausentismo" name="ausentismo" class="form-control select2" style="width: 100%;" onchange="queryActividad();" required>
+                	<option value="0"> </option>
+                    <option value="1">Vacaciones</option>
+                    <option value="2">Permiso</option>
+                    <option value="3">Incapacidad</option>
+                    <option value="4">Evento corporativo</option>
+                    </select>
+                 </div>
+                 </div>
+                 
+                    <div class="form-group">                 
+                  <label>Fecha y hora inicio</label>
+                  <div class="input-group">
+					<div class="input-group-addon">
+                  <i class="fa fa-calendar"></i> 
+                  </div>
+                  <input id="fecha_inicio" name="fecha_inicio" required value="" type="datetime-local" class="form-control"  >
+                </div>
+                </div>
+                
+                
+                  <div class="form-group">                
+                  <label>Fecha y hora fin</label>
+                  <div class="input-group">
+					<div class="input-group-addon">
+                  <i class="fa fa-calendar"></i> 
+                  </div>
+                  <input id="fecha_fin" name="fecha_fin" required value="" type="datetime-local" class="form-control"  >
+                </div> 
+                </div> 
+                
+                <div class="form-group">                
+                  <label>Comentario</label>
+                  <div class="input-group">
+					<div class="input-group-addon">
+                  <i class="fa fa-commenting"></i> 
+                  </div>
+                  <textarea id="comentario" name="comentario" required class="form-control"></textarea>
+                </div> 
+                </div><br>
+                
+                <button type="submit" class="btn btn-success" style="width:150px;">Guardar</button>
+                
+                <a href="#"><button type="button" class="btn btn-danger" style="width:150px;">Cancelar</button></a>              
+                 </div>			
+			</div>
+			<!-- /.box-body -->
+		</div>
+		<!-- /.box -->
+	</div>
+</form>
