650e1ececbfae2d1325d244b972c2c7554c7d7d1
carlos.lopezm <carlos.lopezm@LMDEADM0326888.da.arus.com.co.>
2017-03-08 13:32:28 -0500

    ausentismos en construcción

--- pages/components/registro_ausentismo.php
+++ pages/components/registro_ausentismo.php
@@ -1,2 +1,10 @@
-<?php
-echo "hola";
+      <div class="error-page">
+		<h2 class="headline text-red">500</h2>
+        <div class="error-content">
+          <h3><i class="fa fa-warning text-red"></i> Sitio en Construcción.</h3>
+          <p>
+            Actualmente estamos desarrollando esta funcionalidad!
+          </p>
+        </div>
+        <!-- /.error-content -->
+      </div>
